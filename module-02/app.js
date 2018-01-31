573dfd056fc70ecfc94358b4f15931b667aa2c73
Artur <33759804+varona50@users.noreply.github.com>
2018-01-31 19:36:09 +0200

    Changes (view) in writeAlgoDemo

--- module-02/app.js
+++ module-02/app.js
@@ -1,8 +1,8 @@
 function writeAlgoDemo() {
     var side_a = Math.random() * 10 + 5;
     var side_b = Math.random() * 10 + 5;
-    document.write("Side a=" + side_a + "Side b=" + side_b);
-    document.write("Обьем куба" + side_a * side_a * side_a);
-    document.write("Площадь квадрата" + side_a * side_a);
-    document.write("Площадь прямоугольника" + side_a * side_b);
+    document.write("<p>Side a= " + side_a + "</p><p>Side b= " + side_b + "</p>");
+    document.write("<p>Обьем куба " + side_a * side_a * side_a + "</p>");
+    document.write("<p>Площадь квадрата " + side_a * side_a + "</p>");
+    document.write("<p>Площадь прямоугольника " + side_a * side_b + "</p>");
 }
