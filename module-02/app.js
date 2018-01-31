aefc605f361e50458d2116be9338c3b0552c2e95
Artur <33759804+varona50@users.noreply.github.com>
2018-01-31 19:43:47 +0200

    Added algo for traingle

--- module-02/app.js
+++ module-02/app.js
@@ -7,4 +7,9 @@ function writeAlgoDemo1() {
     document.write("<p>Площадь прямоугольника " + side_a * side_b + "</p>");
 }
 
-function writeAlgoDemo2() {}
+function writeAlgoDemo2() {
+    var side_a = Math.random() * 10 + 5;
+    var side_b = Math.random() * 10 + 5;
+    document.write("<p>Side a= " + side_a + "</p><p>Side b= " + side_b + "</p>");
+    document.write("<p>Площадь прямоугольного треуголника " + side_a * side_b / 2 + "</p>");
+}
